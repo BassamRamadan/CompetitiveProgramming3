879d9ddc45493821c458f3459850c65629a00ff4
BassamRamadan <36972895+BassamRamadan@users.noreply.github.com>
2019-03-24 09:11:51 -0700

    Update 1121 Subsequence.cpp

--- uva/1121 Subsequence.cpp
+++ uva/1121 Subsequence.cpp
@@ -1,70 +1,58 @@
-//#include<bits/stdc++.h>
-#include <iostream>
-#include <string>
-#include <map>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <sstream>
-#include <iomanip>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <algorithm>
-#include <vector>
-#include <set>
-#include <complex>
-#include <list>
-#include <climits>
-#include <bitset>
+#include <bits/stdc++.h>
+#include <unordered_map>
 
+/*
+**
+Success doesn't come to you ,
+You Go To IT .
+**
+*/
 using namespace std;
 #define all(v)          ((v).begin()), ((v).end())
+#define finish(x) return cout << x << endl, 0;
 #define pb push_back
 #define Compiler_Beso ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define clr(v,d)        memset(v, d, sizeof(v))
 #define endl '\n'
-const double EPS = 1e-12;
 typedef long long ll;
-const double PI = 3.14159265359;
+typedef unsigned long long ull;
 
 
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+
+enum dir       { DOWN = 0, UP, RIGHT, LEFT, DOWN_RIGHT, DOWN_LEFT, UP_RIGHT, UP_LEFT };
+int  dir_r[] = { 1, -1, 0, 0, 1, 1, -1, -1 };
+int  dir_c[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
 
 int main(){
-
 	Compiler_Beso
 
-
-		int n, s;
-	while (cin>>n>>s)
+		ll n, m;
+	while (cin>>n>>m)
 	{
-		vector<int>v(n);
+		vector<ll>v(n);
 		for (int i = 0; i < n; i++)
 			cin >> v[i];
-		ll mnL = 0, mnR = LLONG_MAX;
-		ll l = 0, r = 0,sum=0;
-		bool ok = 0;
+
+		ll l = 0, r = 0, sum = 0, mx = 2e9;
 		while (l<n)
 		{
-			while (r < n && sum <= s)
-			{
+			while (r < n && sum<m ){
 				sum += v[r];
 				r++;
 			}
-			if (sum >= s)
-			{
-				ok = 1;
-				if ((r - l - 1) < (mnR - mnL))
-					mnR = r, mnL = l;
+			if (sum >= m){
+				mx = min(mx, r - l);
 			}
 			sum -= v[l];
 			l++;
 		}
-		
-		cout << (ok?mnR - mnL:0) << endl;
+		if (mx == 2e9)cout << 0 << endl;
+		else
+		cout << mx << endl;
 	}
 
-		
 	return 0;
 }
