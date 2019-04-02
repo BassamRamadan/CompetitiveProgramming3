4cdb6906a78d78d0e5c37f5de18b1d82c1b658c4
BassamRamadan <36972895+BassamRamadan@users.noreply.github.com>
2019-04-02 13:33:08 -0700

    Update UVa - 12032 The Monkey and the Oiled Bamboo.cpp

--- uva/UVa - 12032 The Monkey and the Oiled Bamboo.cpp
+++ uva/UVa - 12032 The Monkey and the Oiled Bamboo.cpp
@@ -1,101 +1,71 @@
-//#include<bits/stdc++.h>
-#include <iostream>
-#include <string>
-#include <map>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <sstream>
-#include <iomanip>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <algorithm>
-#include <vector>
-#include <set>
-#include <complex>
-#include <list>
-#include <climits>
-#include <cctype>
-#include <bitset>
-using namespace std;
+#include <bits/stdc++.h>
+#include <unordered_map>
+
 /*
-***
 **
-*
-It is important to start with easy problems,
-learn from them,
-and work up to harder problems
- 
-***
+Success doesn't come to you ,
+You Go To IT .
 **
-*
 */
+using namespace std;
 #define all(v)          ((v).begin()), ((v).end())
+#define finish(x) return cout << x << endl, 0;
 #define pb push_back
 #define Compiler_Beso ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define clr(v,d)        memset(v, d, sizeof(v))
-const double EPS = 1e-12;
-const double PI = 3.14159265359;
+#define endl '\n'
 typedef long long ll;
-const ll mod = ll(1e6 + 3);
-int n, a[1000000];
-bool solve(int sz)
-{
-	int div = 0;
-	for (int i = 1; i <= n; i++)
-	{
-		div = a[i] - a[i - 1];
-		if (div == sz)
-			--sz;
-		else
-		if (div > sz)
-		{
-			return 0;
-		}
+typedef unsigned long long ull;
+
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+
+enum dir       { DOWN = 0, UP, RIGHT, LEFT, DOWN_RIGHT, DOWN_LEFT, UP_RIGHT, UP_LEFT };
+int  dir_r[] = { 1, -1, 0, 0, 1, 1, -1, -1 };
+int  dir_c[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int n;
+vector<int>v;
+bool ok(ll mid){
+	if (v[0] > mid) return 0;
+	if (v[0] == mid) mid--;
+	for (int i = 1; i < n; i++){
+		if (mid < 0)return 0;
+		if (v[i] - v[i - 1] > mid) return 0;
+		if (v[i]-v[i-1] == mid) mid--;
 	}
-	return 1;
+	return mid >= 0;
 }
-int bin()
-{
-	ll l = 1, r = 1e8,m = 0, ans = 0;
-	while (l<=r)
-	{
-		m = l + (r - l) / 2;
-		bool f = solve(m);
-		if (f)
-		{
-			ans = m;
-			r = m - 1;
-		}
-		else
-			l = m + 1;
-	}
-	return ans;
-}
-int main()
-{
+int main(){
 	Compiler_Beso
- 
- 
- 
- 
-	int q;
-	cin >> q;
-	for (int t = 1; t <= q;t++){
+
+		int t, id = 0;
+	cin >> t;
+	while (t--)
+	{
+	
 		cin >> n;
-		int div = INT_MIN;
-		for (int i = 1; i <= n; i++)
-		{
-			cin >> a[i];
- 
+		v.resize(n);
+		for (int i = 0; i < n; i++){
+			cin >> v[i];
 		}
- 
-		int c=bin();
-	cout << "Case " << t << ": " << c<< endl;
- 
+		ll l = 0, r = 1e18, ans = 1e18;
+		while (l<=r)
+		{
+			ll mid = (l + r) / 2;
+			if (ok(mid)){
+				ans = min(ans, mid);
+				r = mid - 1;
+			}
+			else l = mid + 1;
+		}
+		cout <<"Case "<<++id<<": "<< ans << endl;
 	}
- 
+
+
+
+
+		
 	return 0;
 }
