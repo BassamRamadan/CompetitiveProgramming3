00d2d0c3aa6abe2fe3b56c385ef4baaac6a7d3f9
BassamRamadan <36972895+BassamRamadan@users.noreply.github.com>
2019-04-02 13:07:47 -0700

    Update 11900 Boiled Eggs.cpp

--- uva/11900 Boiled Eggs.cpp
+++ uva/11900 Boiled Eggs.cpp
@@ -1,63 +1,57 @@
-//#include<bits/stdc++.h>
-#include <iostream>
-#include <string>
-#include <map>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <sstream>
-#include <iomanip>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <algorithm>
-#include <vector>
-#include <set>
-#include <complex>
-#include <list>
-#include <climits>
-#include <bitset>
+#include <bits/stdc++.h>
+#include <unordered_map>
 
+/*
+**
+Success doesn't come to you ,
+You Go To IT .
+**
+*/
 using namespace std;
 #define all(v)          ((v).begin()), ((v).end())
+#define finish(x) return cout << x << endl, 0;
 #define pb push_back
 #define Compiler_Beso ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define clr(v,d)        memset(v, d, sizeof(v))
 #define endl '\n'
-const double EPS = 1e-12;
 typedef long long ll;
-const double PI = 3.14159265359;
+typedef unsigned long long ull;
 
 
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+
+enum dir       { DOWN = 0, UP, RIGHT, LEFT, DOWN_RIGHT, DOWN_LEFT, UP_RIGHT, UP_LEFT };
+int  dir_r[] = { 1, -1, 0, 0, 1, 1, -1, -1 };
+int  dir_c[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
 
 int main(){
-
 	Compiler_Beso
 
-
-		int t,c=1;
+		int t, id = 0;
 	cin >> t;
 	while (t--)
 	{
 		int n, p, q;
 		cin >> n >> p >> q;
-		vector<int>v(n);
-		for (int i = 0; i < n; i++)
-			cin >> v[i];
-		sort(all(v));
-		int sum = 0;
-		int i = 0;
-		p = min(p, n);
-		for (; i < p; i++)
-		{
-			if (sum + v[i] <= q)
-				sum += v[i];
-			else
-				break;
+		int ans = 0;
+		for (int i = 0; i < n; i++){
+			int x;
+			cin >> x;
+			if (q - x >= 0 && p){
+				++ans;
+				p--;
+				q -= x;
+			}
 		}
-		cout <<"Case "<<c++<<": "<< i << endl;
+		
+		cout <<"Case "<<++id<<": "<< ans << endl;
 	}
 
+
+
+
+		
 	return 0;
 }
